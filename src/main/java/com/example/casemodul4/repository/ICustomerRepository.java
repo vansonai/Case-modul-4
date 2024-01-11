e6aa397e61802ff6c18cbf41f9e0fa62877d47be
nbt311 <nbtrung0311@gmail.com>
2024-01-11 16:41:52 +0700

    Create Interface ICustomerRepository and IRoleRepository

---
+++ src/main/java/com/example/casemodul4/repository/ICustomerRepository.java
@@ -0,0 +1,8 @@
+package com.example.casemodul4.repository;
+import com.example.casemodul4.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/example/casemodul4/repository/IRoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.casemodul4.repository;
+
+import com.example.casemodul4.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<Role, Long> {
+}
