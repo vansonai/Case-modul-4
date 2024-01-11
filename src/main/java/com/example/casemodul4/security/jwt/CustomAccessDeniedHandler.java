a9de4fbe808136e7c96816fc37622565ff8e6a31
nbt311 <nbtrung0311@gmail.com>
2024-01-11 16:37:36 +0700

    Create Class CustomAccessDeniedHandler

--- src/main/java/com/example/casemodul4/security/jwt/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/casemodul4/security/jwt/CustomAccessDeniedHandler.java
@@ -1,4 +1,17 @@
 package com.example.casemodul4.security.jwt;
 
-public class CustomAccessDeniedHandler {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exc)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("Access Denied!");
+    }
 }
