2c03438348bd705899a357f3be14d99bcb1c95c0
08201356 <nguyenhoainma0613@gmail.com>
2024-01-11 23:47:00 +0700

    thêm về phần product

---
+++ src/main/java/com/example/casemodul4/repository/IProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.casemodul4.repository;
+
+import com.example.casemodul4.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/com/example/casemodul4/service/IProductService.java
@@ -0,0 +1,6 @@
+package com.example.casemodul4.service;
+
+import com.example.casemodul4.model.Product;
+
+public interface IProductService extends IGenerateService<Product> {
+}
