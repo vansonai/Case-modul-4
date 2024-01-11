49f44971e82c8f12b6fde4e7ac1b71bcc8940426
nbt311 <nbtrung0311@gmail.com>
2024-01-11 16:35:08 +0700

    Create Class RestAuthenticationEntryPoint

--- src/main/java/com/example/casemodul4/security/jwt/RestAuthenticationEntryPoint.java
+++ src/main/java/com/example/casemodul4/security/jwt/RestAuthenticationEntryPoint.java
@@ -1,4 +1,17 @@
 package com.example.casemodul4.security.jwt;
 
-public class RestAuthenticationEntryPoint {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Unauthorized");
+    }
 }
