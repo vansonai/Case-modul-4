d4cfd3e183234856d9b273786c77931c35c7316e
vansonai <design97.ae@gmail.com>
2024-01-12 09:02:01 +0700

    Add new customer interface

---
+++ src/main/java/com/example/casemodul4/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.example.casemodul4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class CustomerController {
+//    public ModelAndView listCustomer(){
+//        ModelAndView modelAndView = new ModelAndView("/customer/list");
+//        modelAndView.addObject()
+//    }
+}
---
+++ src/main/java/com/example/casemodul4/service/ICustomerService.java
@@ -0,0 +1,4 @@
+package com.example.casemodul4.service;
+
+public interface ICustomerService {
+}
