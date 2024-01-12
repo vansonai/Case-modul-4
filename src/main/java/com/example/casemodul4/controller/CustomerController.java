74daff85c2a7621f98d419428c9cc75065e49c45
vansonai <design97.ae@gmail.com>
2024-01-12 10:41:33 +0700

    display form update customer

--- src/main/java/com/example/casemodul4/controller/CustomerController.java
+++ src/main/java/com/example/casemodul4/controller/CustomerController.java
@@ -1,11 +1,16 @@
 package com.example.casemodul4.controller;
 
+import com.example.casemodul4.model.Customer;
+import com.example.casemodul4.service.CustomerService;
 import com.example.casemodul4.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/customers")
@@ -18,8 +23,16 @@ public class CustomerController {
         modelAndView.addObject("customers", customerService.findAll());
         return modelAndView;
     }
-//    @GetMapping("/update")
-//    public ModelAndView updateCustomer(){
-//        return new ModelAndView("/customer/update");
-//    }
+    @GetMapping("/update/{id}")
+    public ModelAndView updateCustomer(@PathVariable Long id){
+        Optional<Customer> customer = customerService.findById(id);
+
+        if (customer.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/customer/update");
+            modelAndView.addObject("customers", customer.get());
+            return modelAndView;
+        }else {
+            return new ModelAndView("/error_404");
+        }
+    }
 }
