bae18db2638194cab6df53dc341565e09a5f9d2e
08201356 <nguyenhoainma0613@gmail.com>
2024-01-12 08:29:53 +0700

    thêm sửa xoá phần product

---
+++ src/main/java/com/example/casemodul4/service/impl/ProductService.java
@@ -0,0 +1,35 @@
+package com.example.casemodul4.service.impl;
+
+import com.example.casemodul4.model.Product;
+import com.example.casemodul4.repository.IProductRepository;
+import com.example.casemodul4.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService {
+    @Autowired
+    private IProductRepository iProductRepository;
+
+    @Override
+    public Iterable<Product> findAll() {
+        return iProductRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return iProductRepository.findById(id);
+    }
+
+    @Override
+    public Product save(Product product) {
+        return iProductRepository.save(product);
+    }
+
+    @Override
+    public void delete(Long id) {
+        iProductRepository.deleteById(id);
+    }
+}
